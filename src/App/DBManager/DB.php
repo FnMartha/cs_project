3a0e0107f2d6e44d9a8a99438f6934b8c33ef50f
Frank <fnmartha789@gmail.com>
2017-09-14 15:53:12 +0300

    added attributes to user models

---
+++ src/App/DBManager/DB.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: frank
+ * Date: 9/14/17
+ * Time: 3:42 PM
+ */
+
+namespace App\DBManager;
+class DB
+{
+    /**
+     * @var string
+     */
+    private $databaseName = 'smart_keja';
+    /**
+     * @var string
+     */
+    private $password = '';
+    /**
+     * @var string
+     */
+    private $databaseHost = 'localhost';
+    /**
+     * @var string
+     */
+    private $databaseUser = 'root';
+    /**
+     * @var
+     */
+    private $conn;
+
+    /**
+     * @return null|\PDO
+     */
+    public function connect(){
+        try{
+
+            $this->conn = new \PDO(
+                "mysql:host={$this->databaseHost};
+                 dbname={$this->databaseName}",
+                $this->databaseUser,
+                $this->password
+            );
+
+            return $this->conn;
+
+        } catch (\PDOException $e){
+            echo $e->getMessage();
+            return null;
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function closeConnection(){
+        $this->conn = null;
+        return true;
+    }
+}
+
--- src/App/Models/User.php
+++ src/App/Models/User.php
@@ -11,5 +11,108 @@ namespace App\Models;
 
 class User
 {
+    private $id;
+    private $username;
+    private $email;
+    private $password;
+    private $accountType;
+    private $phoneNumber;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAccountType()
+    {
+        return $this->accountType;
+    }
+
+    /**
+     * @param mixed $accountType
+     */
+    public function setAccountType($accountType)
+    {
+        $this->accountType = $accountType;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * @param mixed $phoneNumber
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
 
 }
