8cd26ca329410a8f45b2a71c28c1c299db23b5d8
Frank <fnmartha789@gmail.com>
2017-09-14 17:45:42 +0300

    completed CRUDE FOR house and houserbooking models

--- src/App/Controllers/HouseBookingController.php
+++ src/App/Controllers/HouseBookingController.php
@@ -9,7 +9,143 @@
 namespace App\Controllers;
 
 
-class HouseBookingController
+use App\AppInterface\HouseBookingInterface;
+use App\DBManager\DB;
+use App\Models\HouseBooking;
+
+class HouseBookingController implements HouseBookingInterface
 {
+    public function create(HouseBooking $booking)
+    {
+        try {
+            $db = new DB();
+            $conn = $db->connect();
+            $stmt = $conn->prepare("INSERT INTO house_bookings(client_id, amount_paid, amount_due, house_id, date_booked) 
+                    VALUES (:client_id, :amount_paid, :amount_due, :house_id, :date_booked)");
+
+            $stmt->bindValue(":client_id", $booking->getClientId());
+            $stmt->bindValue(":amount_paid", $booking->getAmountPaid());
+            $stmt->bindValue(":amount_due", $booking->getAmountDue());
+            $stmt->bindValue(":house_id", $booking->getHouseId());
+            $stmt->bindValue(":date_booked", $booking->getDateBooked());
+
+            $created = $stmt->execute();
+            if ($created) {
+                return true;
+            } else {
+                return ['error' => "Error Occurred. House Booking No Saved"];
+            }
+
+
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            return [
+                "error" => $e->getMessage()
+            ];
+        }
+    }
+
+    public function update(HouseBooking $booking, $id)
+    {
+        try {
+            $db = new DB();
+            $conn = $db->connect();
+            $stmt = $conn->prepare("UPDATE house_bookings SET 
+                                client_id=:client_id,
+                                amount_paid=:amount_paid,
+                                amount_due=:amount_due, 
+                                house_id=:house_id,
+                                date_booked=:date_booked
+                                WHERE id=:id
+                                ");
+
+            $stmt->bindParam(":id", $id);
+            $stmt->bindValue(":client_id", $booking->getClientId());
+            $stmt->bindValue(":amount_paid", $booking->getAmountPaid());
+            $stmt->bindValue(":amount_due", $booking->getAmountDue());
+            $stmt->bindValue(":house_id", $booking->getHouseId());
+            $stmt->bindValue(":date_booked", $booking->getDateBooked());
+
+            $created = $stmt->execute();
+            if ($created) {
+                return true;
+            } else {
+                return ['error' => "Error Occurred. House Booking No Saved"];
+            }
+
+
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            return [
+                "error" => $e->getMessage()
+            ];
+        }
+    }
+
+    public static function getId($id)
+    {
+        $db = new DB();
+        $conn = $db->connect();
+        try{
+            $stmt = $conn->prepare("SELECT * FROM house_bookings WHERE id:=id");
+            $stmt->bindParam(":id", $id);
+            if($stmt->execute() && $stmt->rowCount() == 1){
+                return $stmt->fetchAll();
+            }else{
+                return [
+                    "error"=> "No Data found"
+                ];
+            }
+        }catch (\PDOException $e){
+            return [
+                "error"=>$e->getMessage()
+            ];
+        }
+    }
+
+    public static function delete($id)
+    {
+        $db = new DB();
+        $conn = $db->connect();
+        try{
+            $stmt = $conn->prepare("DELETE FROM house_bookings WHERE id=:id");
+            $stmt->bindParam(":id", $id);
+            if($stmt->execute()){
+                return true;
+            }else{
+                return [
+                    "error" => "Error! Failed to delete the house  try again later"
+                ];
+
+            }
+        }catch (\PDOException $e){
+            return [
+                "error"=>$e->getMessage()
+            ];
+        }
+    }
+
+
+    public static function all()
+    {
+        $db = new DB();
+        $conn = $db->connect();
+        try{
+            $stmt = $conn->prepare("SELECT * FROM house_bookings WHERE 1");
+
+            if($stmt->execute() && $stmt->rowCount() == 1){
+                return $stmt->fetchAll();
+            }else{
+                return [
+                    "error"=> "No Data found"
+                ];
+            }
+        }catch (\PDOException $e){
+            return [
+                "error"=>$e->getMessage()
+            ];
+        }
+    }
+
 
 }
--- src/App/Controllers/HouseController.php
+++ src/App/Controllers/HouseController.php
@@ -9,7 +9,149 @@
 namespace App\Controllers;
 
 
-class HouseController
+use App\AppInterface\HouseInterface;
+use App\DBManager\DB;
+use App\Models\House;
+
+class HouseController implements HouseInterface
 {
+    public function create(House $house)
+    {
+        try {
+            $db = new DB();
+            $conn = $db->connect();
+            $stmt = $conn->prepare("INSERT INTO houses(owned_by, house_category, location, min_price, max_price, image1, image2, image3, image4, image5, status) 
+                    VALUES (:owned_by, :house_category, :location, :min_price, :max_price, :image1, :image2, :image3, :image4, :image5, :status)");
+
+            $stmt->bindValue(":owned_by", $house->getOwnedBy());
+            $stmt->bindValue(":house_category", $house->getHouserCategory());
+            $stmt->bindValue(":location", $house->getLocation());
+            $stmt->bindValue(":min_price", $house->getMinPrice());
+            $stmt->bindValue(":max_price", $house->getMaxPrice());
+            $stmt->bindValue(":image1", $house->getImageOne());
+            $stmt->bindValue(":image2", $house->getImageTwo());
+            $stmt->bindValue(":image3", $house->getImageThree());
+            $stmt->bindValue(":image4", $house->getImageFour());
+            $stmt->bindValue(":image5", $house->getImageFive());
+            $stmt->bindValue(":status", $house->getStatus());
+            $created = $stmt->execute();
+            if ($created) {
+                return true;
+            } else {
+                return ['error' => "Error Occurred. House Owner Not Registered"];
+            }
+
+
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            return [
+                "error" => $e->getMessage()
+            ];
+        }
+    }
+
+    public function update(House $house, $id)
+    {
+        try {
+            $db = new DB();
+            $conn = $db->connect();
+            $stmt = $conn->prepare("UPDATE houses SET owned_by=:owned_by, house_category=:house_category, location=:location, min_price=:min_price,
+                                            max_price=:max_price, image1=:image1,
+                                            image2=:image2, image3=:image3, 
+                                            image4=:image4, image5=:image5, 
+                                            status=:status WHERE id=:id "
+            );
+            $stmt->bindParam(":id",$id);
+            $stmt->bindValue(":owned_by", $house->getOwnedBy());
+            $stmt->bindValue(":house_category", $house->getHouserCategory());
+            $stmt->bindValue(":location", $house->getLocation());
+            $stmt->bindValue(":min_price", $house->getMinPrice());
+            $stmt->bindValue(":max_price", $house->getMaxPrice());
+            $stmt->bindValue(":image1", $house->getImageOne());
+            $stmt->bindValue(":image2", $house->getImageTwo());
+            $stmt->bindValue(":image3", $house->getImageThree());
+            $stmt->bindValue(":image4", $house->getImageFour());
+            $stmt->bindValue(":image5", $house->getImageFive());
+            $stmt->bindValue(":status", $house->getStatus());
+            $created = $stmt->execute();
+            if ($created) {
+                return true;
+            } else {
+                return ['error' => "Error Occurred. House Owner Not Registered"];
+            }
+
+
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            return [
+                "error" => $e->getMessage()
+            ];
+        }
+    }
+
+    public static function getId($id)
+    {
+        $db = new DB();
+        $conn = $db->connect();
+        try{
+            $stmt = $conn->prepare("SELECT * FROM houses WHERE id:=id");
+            $stmt->bindParam(":id", $id);
+            if($stmt->execute() && $stmt->rowCount() == 1){
+                return $stmt->fetchAll();
+            }else{
+                return [
+                    "error"=> "No Data found"
+                ];
+            }
+        }catch (\PDOException $e){
+            return [
+                "error"=>$e->getMessage()
+            ];
+        }
+    }
+
+    public static function delete($id)
+    {
+        $db = new DB();
+        $conn = $db->connect();
+        try{
+            $stmt = $conn->prepare("DELETE FROM houses WHERE id=:id");
+            $stmt->bindParam(":id", $id);
+            if($stmt->execute()){
+                return true;
+            }else{
+                return [
+                    "error" => "Error! Failed to delete the house  try again later"
+                ];
+
+            }
+        }catch (\PDOException $e){
+            return [
+                "error"=>$e->getMessage()
+            ];
+        }
+    }
+
+    public static function all()
+    {
+        $db = new DB();
+        $conn = $db->connect();
+        try{
+            $stmt = $conn->prepare("SELECT * FROM houses WHERE 1");
+
+            if($stmt->execute() && $stmt->rowCount() == 1){
+                return $stmt->fetchAll();
+            }else{
+                return [
+                    "error"=> "No Data found"
+                ];
+            }
+        }catch (\PDOException $e){
+            return [
+                "error"=>$e->getMessage()
+            ];
+        }
+    }
+
 
 }
